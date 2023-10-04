ce9860635ba985b5303f7a4cb4613cba7c7b7fe8
jcat124 <nahal.jodhsingh@gmail.com>
2023-10-04 00:09:58 -0700

    sol

--- src/main/java/primedirective/PrimeFactorSequence.java
+++ src/main/java/primedirective/PrimeFactorSequence.java
@@ -28,7 +28,22 @@ public class PrimeFactorSequence {
     public List<Integer> primeFactorSequence() {
         List<Integer> seq = new ArrayList<>();
         // TODO: Implement this method
+        for (int i = 0; i <= upperBound; i++) {
+            seq.add(prime(i));
+        }
         return seq;
+    }
+
+    public int prime(int i){
+        if(primes.contains(i)){
+            return 1;
+        }
+        for (int k = 2; k <= i; k++) {
+            if(i%k == 0){
+                return prime(i/k) + prime(k);
+            }
+        }
+        return 0;
     }
 
     /**
@@ -41,7 +56,13 @@ public class PrimeFactorSequence {
      */
     public List<Integer> numbersWithMPrimeFactors(int m) {
         List<Integer> seq = new ArrayList<>();
+        List<Integer> s = primeFactorSequence();
         // TODO: Implement this method
+        for (int i = 0; i <= upperBound; i++) {
+            if(s.get(i) == m){
+                seq.add(i);
+            }
+        }
         return seq;
     }
 
@@ -60,6 +81,18 @@ public class PrimeFactorSequence {
     public List<IntPair> numbersWithMPrimeFactorsAndSmallGap(int m, int gap) {
         List<IntPair> listOfPairs = new ArrayList<>();
         // TODO: Implement this method
+        List<Integer> k = numbersWithMPrimeFactors(m);
+        IntPair p;
+        System.out.println(k);
+        for (int i = 0; i < k.size(); i++) {
+            for (int j = i+1; j < k.size(); j++) {
+                if(k.get(j)-k.get(i) <= gap){
+                    p = new IntPair(k.get(i), k.get(j));
+                    listOfPairs.add(p);
+                    System.out.println(1);
+                }
+            }
+        }
         return listOfPairs;
     }
 
